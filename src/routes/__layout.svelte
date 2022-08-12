fcb5c1b9532d0c116852ba8b44c90ffdb07e2f05
Thiago Cruz Santos <thiagocruzsantos@gmail.com>
2022-08-12 18:21:51 -0300

    making it tick

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '$styles/app.scss';
+</script>
+
+<svelte:head>
+	<style>
+		@import url('https://use.typekit.net/ymn3wlm.css');
+	</style>
+</svelte:head>
+<slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { decreaser } from '$lib/transitions';
+	import { onMount } from 'svelte';
+	let counter = 175;
+	let unique = {};
+	onMount(() => {
+		unique = {};
+		document.getElementById('seconds')?.focus();
+	});
+</script>
+
+{#key unique}
+	<h1 in:decreaser>{counter}</h1>
+{/key}
+<input
+	type="text"
+	id="seconds"
+	bind:value={counter}
+	on:keypress={(e) => {
+		if (isNaN(parseInt(e.key)) && e.key !== 'Backspace') e.preventDefault();
+	}}
+	on:keyup={() => {
+		unique = {};
+	}}
+/>
