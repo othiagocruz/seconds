d8d1fb0669ba34c45b5cd47809f2d635c210cc28
Thiago Cruz Santos <thiagocruzsantos@gmail.com>
2022-08-12 20:39:55 -0300

    favicon

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,6 +3,8 @@
 </script>
 
 <svelte:head>
+	<title>seconds</title>
+	<meta name="description" value="A tool for visualizing seconds" />
 	<style>
 		@import url('https://use.typekit.net/ymn3wlm.css');
 	</style>
