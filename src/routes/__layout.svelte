861a05e339ad6745416ce3e5a47fe5c1f77c8aa1
Thiago Cruz Santos <thiagocruzsantos@gmail.com>
2022-08-13 12:54:26 -0300

    revert to link

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,10 @@
 <script>
-  import "$styles/fonts.scss";
   import "$styles/app.scss";
 </script>
 
 <svelte:head>
+  <link rel="preload" as="style" href="https://use.typekit.net/ymn3wlm.css" />
+  <link rel="stylesheet" href="https://use.typekit.net/ymn3wlm.css" />
   <title>seconds</title>
   <meta name="description" value="A tool for visualizing seconds" />
 </svelte:head>
